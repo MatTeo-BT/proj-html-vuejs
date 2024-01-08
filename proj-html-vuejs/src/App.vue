f9919b8891020d0ba0fb529b5c990573f802d05e
MatTeo-BT <aboutmet@gmail.com>
2024-01-08 12:02:56 +0100

    implementate sezioni

--- proj-html-vuejs/src/App.vue
+++ proj-html-vuejs/src/App.vue
@@ -1,11 +1,30 @@
-<script>
-
-</script>
-
 <template>
-  <div>
+  <AppHeader />
 
-  </div>
+  <AppMain />
+
+  <AppFooter />
 </template>
+  
+  
+<script>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+import AppFooter from './components/AppFooter.vue';
 
-<style></style>
+
+export default {
+  components: {
+    AppHeader,
+    AppMain,
+    AppFooter
+  }
+
+}
+</script>
+  
+  
+  
+<style lang="scss">
+@use './style/general.scss' as *;
+</style>
